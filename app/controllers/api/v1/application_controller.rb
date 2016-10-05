7e3435f0f927b05893d28f5624b7d634cdcffc30
Mahesh <mahesh@>
2016-10-05 17:34:52 +0530

    done authentication via access toke for the api

--- app/controllers/api/v1/application_controller.rb
+++ app/controllers/api/v1/application_controller.rb
@@ -5,9 +5,14 @@ module Api
  			# For APIs, you may want to use :null_session instead.
   			protect_from_forgery with: :exception
 
+  			before_action :authenticate_via_access_token
   			
 
-
+  			private
+  			def authenticate_via_access_token
+  				@user = User.find_by_access_token(params[:access_token])
+  				head :unauthorized unless @user
+  			end
 		end
 	end
 end
